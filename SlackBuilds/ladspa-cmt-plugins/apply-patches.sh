2062fd632aa627506d4bdf7d14c8c046535bd41a
Danny Schmarsel <danny@schmarsel.de>
2016-05-05 17:38:44 +0200

    Update ladspa-cmt SlackBuild
    This uses up-to-date build mechanics and also removes some patches,
    so we have a standard cmt build without any changes.

--- SlackBuilds/ladspa-cmt-plugins/apply-patches.sh
+++
@@ -1,6 +0,0 @@
-  
-SB_PATCHDIR=${CWD}/patches
-
-zcat ${SB_PATCHDIR}/${PNAME}-1.15-addnoise.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/${PNAME}-1.15-dontdenormal.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/${PNAME}-1.15-nostrip.patch.gz | patch -p1 -E --backup --verbose || exit 1
