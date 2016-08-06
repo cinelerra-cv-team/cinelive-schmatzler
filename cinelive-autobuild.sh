32f8b7710fb365a8a8cf4450f4fd0c3df8877c48
Danny Schmarsel <danny@schmarsel.de>
2016-08-06 09:48:31 +0200

    Switch to d-m-Y for autobuild
    ...because I hate the US time format. Never know if date or month comes first.

--- cinelive-autobuild.sh
+++ cinelive-autobuild.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-_now=$(date +"%m_%d_%Y")
+_now=$(date +"%d_%m_%Y")
 _file="/home/schmatzler/Desktop/cinelive-xfce-$_now.iso"
 echo "Starting autobuild to $_file..."
 ./make_slackware_live.sh -O "$_file"
